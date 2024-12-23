a427ec0de5048bb8a4fc2bf0d170b1b4a629588b
ihateyou <czumbilkonrad2009@gmail.com>
2024-12-23 21:48:23 +0100

    Create Server.java

---
+++ src/main/java/net/server/Server.java
@@ -0,0 +1,38 @@
+package net.server;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+
+
+public class Server {
+
+
+  
+    private static final int PORT = 8080;
+    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+
+  
+    public static void main(String[] args) {
+        System.out.println("Server is starting on port " + PORT);
+
+
+      
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress());
+                
+
+
+              
+                executorService.submit(new ClientHandler(clientSocket));
+            }
+
+          
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
