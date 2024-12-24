3b7782207f66758bdef507a71d4bfdbd6945736e
ihateyou <czumbilkonrad2009@gmail.com>
2024-12-24 21:52:52 +0100

    Update Server.java

--- src/main/java/net/server/Server.java
+++ src/main/java/net/server/Server.java
@@ -1,36 +1,26 @@
 package net.server;
-import java.io.*;
-import java.net.*;
-import java.util.concurrent.*;
 
-
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
+    private static final int PORT = 1111;
+    private static final int THREAD_POOL_SIZE = 10;
+    private static ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
-  
-    private static final int PORT = 1111; // ;)
-    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
-
-
-  
     public static void main(String[] args) {
         System.out.println("Server is starting on port " + PORT);
 
-
-      
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Client connected: " + clientSocket.getInetAddress());
-                
-
-
-              
                 executorService.submit(new ClientHandler(clientSocket));
             }
-
-          
         } catch (IOException e) {
             e.printStackTrace();
         }
